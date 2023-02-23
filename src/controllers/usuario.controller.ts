497d4dd9e2c83bdbc34c464b0d451c98dd70f72b
Davi Gomes <gdavi989@gmail.com>
2023-02-23 17:54:05 -0300

    alterando

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -86,6 +86,7 @@ export class UsuarioController {
                erro.status = 401;
                throw erro;
             }
+            
          }
       } catch (error) {
          var status = error.status;
